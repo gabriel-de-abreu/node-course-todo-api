2d0a466aec4a35094d72e76152bfcbf4362bac04
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-08 19:42:18 -0300

    get all todos

--- server/server.js
+++ server/server.js
@@ -9,12 +9,24 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//Insert new todo
 app.post('/todos',(req,res)=>{
     var todo=new Todo({
         text: req.body.text
     });
     todo.save().then((doc)=>{
         res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+//Get all todos
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({
+            todos
+        })
     },(e)=>{
         res.status(400).send(e);
     });
