30ba2b8dc986903c0be48957ec25ab2d1c4883fb
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-08 19:01:28 -0300

    post route and refactoring

--- server/server.js
+++ server/server.js
@@ -1,49 +1,25 @@
-var mongoose= require('mongoose');
+var express = require('express');
+var bodyParser =require('body-parser');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User}= require('./models/user');
 
-var Todo = mongoose.model('Todo',{
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim:true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo=new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
 });
 
-var User= mongoose.model('User',{
-    email:{
-        type: String,
-        required: true,
-        minlength:1,
-        trim:true
-    }
+app.listen(3000,()=>{
+    console.log('Started on port 3000');
 });
-
-var user = new User({
-    email:"batata@batata.com"
-});
-user.save();
-
-// var todo= new Todo({
-//     text: ' Drink water   ',
-//     completed:true,
-//     completedAt: 123
-// });
-// todo.save().then((doc)=>{
-//     console.log('Saved todo',doc);
-// },(e)=>{
-//     console.log('Unable to save',e);
-// });
-
-// //save new 
-// todo.save();
